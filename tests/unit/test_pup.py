7c0f2ccc97fc51242649526b316bc13aca424669
mattjmcnaughton <mattjmcnaughton@gmail.com>
2017-07-30 11:14:29 -0400

    Add support for installing python and ansible deps
    `pups` can now specify python and ansible dependencies using
    `requirements.{txt,yml}`. We will run the relevant `install` command on
    these files.
    
    We made the decision to not try and implement the virtualenv for the
    user when running install. At least initially, it will be the
    responsibility of the user to ensure they've activated the proper
    virtualenv when running sheepdog. We do provide a mechanism for
    activating a virtualenv during the cron run.

--- tests/unit/test_pup.py
+++ tests/unit/test_pup.py
@@ -1,10 +1,13 @@
+import mock
 import os
 import shutil
 import tempfile
 import unittest
 
 from sheepdog.config import Config
-from sheepdog.pup import FsPup, GalaxyPup, GitPup, Pup, PupfileEntry
+from sheepdog.pup import (AnsibleDependencies, FsPup, GalaxyPup, GitPup,
+                          Pup, PupDependencies, PupfileEntry,
+                          PythonDependencies)
 
 
 class PupTestCase(unittest.TestCase):
@@ -129,3 +132,78 @@ class FsPupTestCase(unittest.TestCase):
         pup_copy_exists_in_kennel = os.path.isdir(expected_pup_location)
 
         self.assertTrue(pup_copy_exists_in_kennel)
+
+
+class PupDependenciesTestCase(unittest.TestCase):
+    def setUp(self, *args, **kwargs):
+        super(PupDependenciesTestCase, self).setUp(*args, **kwargs)
+
+        Config.clear_config_singleton()
+        Config.initialize_config_singleton()
+
+    def test_create_from_dep_file_path(self):
+        filename_to_expected_type = {
+            'requirements.txt': PythonDependencies,
+            'requirements.yml': AnsibleDependencies
+        }
+
+        for filename, expected_type in filename_to_expected_type.iteritems():
+            self.assertIsInstance(
+                PupDependencies.create_from_dep_file_path(filename),
+                expected_type
+            )
+
+
+class PythonDependenciesTestCase(unittest.TestCase):
+    def setUp(self, *args, **kwargs):
+        super(PythonDependenciesTestCase, self).setUp(*args, **kwargs)
+
+        Config.clear_config_singleton()
+        Config.initialize_config_singleton()
+
+    @mock.patch('subprocess.check_call')
+    def test_install(self, mock_check_call):
+        python_dep_file_path = '/tmp/requirements.txt'
+
+        deps = PupDependencies.create_from_dep_file_path(python_dep_file_path)
+        deps.install()
+
+        self.assertEqual(mock_check_call.call_count, 1)
+
+        check_call_args, check_call_kwargs = mock_check_call.call_args
+        pip_cmd = check_call_args[0]
+
+        for cmd_line in {'pip', '-r {}'.format(python_dep_file_path)}:
+            self.assertIn(cmd_line, pip_cmd)
+
+
+class AnsibleDependenciesTestCase(unittest.TestCase):
+    def setUp(self, *args, **kwargs):
+        super(AnsibleDependenciesTestCase, self).setUp(*args, **kwargs)
+
+        Config.clear_config_singleton()
+        Config.initialize_config_singleton()
+
+    @mock.patch('subprocess.check_call')
+    def test_install(self, mock_check_call):
+        ansible_dep_file_path = '/tmp/requirements.yml'
+
+        deps = PupDependencies.create_from_dep_file_path(ansible_dep_file_path)
+        deps.install()
+
+        self.assertEqual(mock_check_call.call_count, 1)
+
+        check_call_args, check_call_kwargs = mock_check_call.call_args
+        ansible_galaxy_cmd = check_call_args[0]
+
+        config = Config.get_config_singleton()
+
+        expected_cmds = {
+            'ansible-galaxy',
+            'install',
+            '-r {}'.format(ansible_dep_file_path),
+            '-p {}'.format(config.get('abs_kennel_roles_dir'))
+        }
+
+        for cmd_line in expected_cmds:
+            self.assertIn(cmd_line, ansible_galaxy_cmd)
