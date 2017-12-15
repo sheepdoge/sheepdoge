a5352c7d61d403d32c50579f0766a1fbfa141498
mattjmcnaughton <mattjmcnaughton@gmail.com>
2017-12-15 07:34:09 -0500

    Release 0.1.4
    I want to start experimenting with some of the new features I've
    developed, so release 0.1.4.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 GITHUB_URL = 'https://github.com/mattjmcnaughton/sheepdoge'
 
 # @TODO(mattjmcnaughton) determine this value dynamically.
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 setup(
     name='sheepdoge',
